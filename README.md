2451b98419a5924efce678a64119723fae8e8248
Roman Lysiuk <roma.freefox@gmail.com>
2020-12-30 12:04:49 +0200

    HCleaner - add sources

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# hcleaner_python
+# HCleander
+
+HCleander is a simple bot to clean messages to and from some annoying bots.
+
+### Installation
+HCleander requires Python 3 to run.
+
+  - Get API ID and API HASH on the site https://my.telegram.org/
+  - Install the dependencies
+  - Create configuration file `config.py` based on example `config/config_example.py`
+  - Start the service
+  
+```sh
+$ git clone https://github.com/FreeFox/hcleaner_python.git
+$ cd hcleaner_python
+$ pip3 install -r requirements.txt
+$ cp config/config_example.py config/config.py
+$ nano config/config.py
+$ python3 main.py
+```
+
+License
+----
+
+MIT
---
+++ main.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+from telethon import TelegramClient, events
+from config import api_hash, api_id
+import argparse
+import asyncio
+from periodic import Periodic
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--api_id", required=False, help="user api ID", type=str, default=api_id)
+parser.add_argument("--api_hash", required=False, help="user api Hash", type=str, default=api_hash)
+
+args = parser.parse_args()
+messages_to_delete = []
+
+
+async def periodically():
+    if len(messages_to_delete):
+        not_old_enough = []
+        print("queue size: ", str(len(messages_to_delete)), " item(s)")
+        utcnow = datetime.utcnow()
+        for msg in messages_to_delete:
+            chat_id = msg['chat']
+            message_id = msg['message']
+            delta = utcnow - datetime.strptime(msg['date'], "%Y-%m-%d %H:%M:%S")
+            if delta.seconds > 30:
+                await client.delete_messages(chat_id, message_id)
+                print(datetime.now(), 'Done!')
+            else:
+                not_old_enough.append(msg)
+        messages_to_delete.clear()
+        messages_to_delete.extend(not_old_enough)
+
+
+async def main():
+    p = Periodic(10, periodically)
+    await p.start()
+
+
+loop = asyncio.get_event_loop()
+loop.create_task(main())
+client = TelegramClient("Cleaner", args.api_id, args.api_hash, loop=loop)
+
+
+@client.on(events.NewMessage(pattern=r'(?i)^\/(achievements|drochnut|topd|topdall|topdd|help|dice|case|use)'))
+async def user_command_handler(event):
+    messages_to_delete.append({
+        "message": event.message.id,
+        "chat": event.chat.id,
+        "date": event.message.date.strftime("%Y-%m-%d %H:%M:%S")
+    })
+    print("[New event] Queue size: ", str(len(messages_to_delete)), " item(s)")
+
+
+@client.on(events.NewMessage(from_users=1303228016))
+async def bot_message_handler(event):
+    messages_to_delete.append({
+        "message": event.message.id,
+        "chat": event.chat.id,
+        "date": event.message.date.strftime("%Y-%m-%d %H:%M:%S")
+    })
+    print("[New event] Queue size: ", str(len(messages_to_delete)), " item(s)")
+
+
+client.start()
+client.run_until_disconnected()
+
