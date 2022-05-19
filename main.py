cd45b472151730f2b1a1bf1a104de8c4965ad8d5
Roman Lysiuk <roman@UA-RLYSIUK2.local>
2022-05-19 10:20:06 +0300

    add kraft28_bot

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ loop.create_task(main())
 client = TelegramClient("Cleaner", args.api_id, args.api_hash, loop=loop)
 
 
-@client.on(events.NewMessage(pattern=r'(?i)^\/(achievements|drochnut|topd|topdall|topdd|help|dice|case|use|keys|shop|trande|bonuscode|inventory|newcase|rr|give|donate|dick@xyu_epta_bot|top@xyu_epta_bot)'))
+@client.on(events.NewMessage(pattern=r'(?i)^\/(achievements|drochnut|topd|topdall|topdd|help|dice|case|use|keys|shop|trande|bonuscode|inventory|newcase|rr|give|donate|dick|top)'))
 async def user_command_handler(event):
     messages_to_delete.append({
         "message": event.message.id,
@@ -52,7 +52,7 @@ async def user_command_handler(event):
     print("[New event] Queue size: ", str(len(messages_to_delete)), " item(s)")
 
 
-@client.on(events.NewMessage(from_users=1303228016))
+@client.on(events.NewMessage(from_users=[1303228016,539991741]))
 async def bot_message_handler(event):
     messages_to_delete.append({
         "message": event.message.id,
